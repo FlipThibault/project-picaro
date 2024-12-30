5d58def21690849f9ae62137e358dec994630d3e
Philippe Thibault <philippe.thibault@wahi.com>
2024-12-29 19:47:04 -0500

    WIP: initial updates for menu

---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void LoadSceneAsync(string Map)
+    {
+        SceneManager.LoadScene(Map);
+    }
+}
